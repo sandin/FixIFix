2b7608fd75b375c9a40d9c0e9bd51bdd02393676
lds <lds2012@gmail.com>
2022-03-04 17:42:01 +0800

    import

---
+++ PatchReader.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FixIFix
+{
+    class PatchReader
+    {
+
+        public PatchReader(string patchfile)
+        {
+
+        }
+
+        public bool Read()
+        {
+            return false; // TODO
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace FixIFix
+{
+    class Program
+    {
+        static void PrintUsage()
+        {
+            Console.WriteLine("Usage:");
+            Console.WriteLine("FixIFix.exe dumppatch <patch_file>");
+            Console.WriteLine("            dumpdll <assmbly_path>");
+            Console.WriteLine("            check <patch_file> <apk_file>");
+        }
+
+        static void Main(string[] args)
+        {
+            if (args.Length < 2)
+            {
+                PrintUsage();
+                return;
+            }
+            string command = args[0];
+            string filepath = args[1];
+            if (command == "dumppatch")
+            {
+                DumpPatch(filepath);
+            } else if (command == "dumpdll")
+            {
+                DumpDll(filepath);
+            } else if (command == "check") 
+            {
+                if (args.Length < 3)
+                {
+                    PrintUsage();
+                    return;
+                }
+                string apkFile = args[2];
+                Check(filepath, apkFile);
+            } else
+            {
+                PrintUsage();
+            }
+        }
+
+        static void DumpPatch(string filepath)
+        {
+            PatchReader reader = new PatchReader(filepath);
+            reader.Read();
+        }
+
+        static void DumpDll(string filepath)
+        {
+            // TODO:
+        }
+
+        static void Check(string patchFile, string apkFile)
+        {
+            // TODO:
+        }
+    }
+}
